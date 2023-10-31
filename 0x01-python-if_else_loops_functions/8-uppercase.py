105da9cf6ef83ab1bcc61dfcc125aba0f8930a40
daveyhmariam <daveyhmariam@gmail.com>
2023-11-01 00:29:58 +0300

    check letter case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for a in str:
-        if ord(a) in range(65, 91):
-            print(f"{chr(ord(a) + 32)}", end='')
-        else:
-            print(f"{a}", end='')
-    print(f"\n")
+        if ord(a) >= 97 and ord(a) <= 122:
+            a = chr(ord(a) - 32)
+        print("{}".format(a), end='')
+    print("\n")
+
