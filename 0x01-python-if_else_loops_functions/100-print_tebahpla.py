b0feead83c15410fa3120e59275d6ce9307349e3
daveyhmariam <daveyhmariam@gmail.com>
2023-11-01 00:47:02 +0300

    reverse alphabet

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,9 +3,7 @@
 i = 122
 for r in range(0, 26):
     if i % 2 == 0:
-        print("{}".format(chr(i)), end ='')
+        print("{}".format(chr(i)), end='')
     if i % 2 != 0:
         print("{}".format(chr(i-32)), end='')
     i -= 1
-
-
