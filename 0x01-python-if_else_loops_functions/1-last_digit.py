316e83bab59d123634ed94fead2529032e35d1d1
daveyhmariam <daveyhmariam@gmail.com>
2023-10-31 23:32:37 +0300

    assign number to a variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,8 @@ prints last digit
 """
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
+if number < 0:
+    last_digit *= -1
 if last_digit < 6 and last_digit != 0:
     print(f"Last digit of {number} is {last_digit} \
 and is less than 6 and not 0")
