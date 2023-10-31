933dbfa0551ba30fc1a4dad7f0bcfe7513452053
daveyhmariam <daveyhmariam@gmail.com>
2023-11-01 00:01:48 +0300

    print_numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 98):
-    print("{:02}, ".format(i), end="")
+for i in range(0, 100):
+    print("{:02}{} ".format(i, "," if i!=99 else ""), end="")
 print(f"\n")
