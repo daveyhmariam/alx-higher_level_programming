fda9e015d38d9d58fbbe42004c51f1975e0f2619
daveyhmariam <daveyhmariam@gmail.com>
2023-11-01 07:54:38 +0300

    insert_number in linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,27 +12,40 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new, *newhead;
+
 	new = (listint_t *)malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
 	new->next = NULL;
 	newhead = *head;
+	if (new->next == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	if (new->n < newhead->n)
+	{
+		new->next = newhead;
+		*head = new;
+		return (new);
+	}
 	if (newhead->n > new->n)
+	{
+		new->next = newhead;
+
+		return (new);
+	}
+	while (newhead->next != NULL)
+	{
+		if ((newhead->next)->n > new->n)
 		{
-			new->next = newhead;
+			new->next = newhead->next;
+			newhead->next = new;
 			return (new);
 		}
-	while (newhead->next != NULL)
-		{
-			if ((newhead->next)->n > new->n)
-			{
-				new->next = newhead->next;
-				newhead->next = new;
-				return (new);
-			}
 			newhead = newhead->next;
 		}
 	newhead->next = new;
-	return (new);	
+	return (new);
 }
