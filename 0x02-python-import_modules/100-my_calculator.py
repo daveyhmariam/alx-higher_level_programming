bdd3a3bc73e40dcf0df3c6d3e40af030033caacd
daveyhmariam <daveyhmariam@gmail.com>
2023-11-02 20:20:20 +0300

    simple calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     if argv[2] in op:
         a = int(argv[1])
         b = int(argv[3])
-        print("{} {} {} = {}".format(a, argv[2], b, op[argv[2]](a, b)))
+        print(f"{a} {argv[2]} {b} = {op[argv[2]](a, b)}")
     else:
         print(f"Unknown operator. Available operators: +, -, * and /")
