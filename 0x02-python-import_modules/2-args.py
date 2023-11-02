82c09af0752f99c71f11edc390fb70d95a9a9bf4
daveyhmariam <daveyhmariam@gmail.com>
2023-11-02 19:37:51 +0300

    adding function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,9 @@ if __name__ == "__main__":
     """
     from sys import argv
     if len(argv) == 1:
-        print("{} argumnt:".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
     else:
         print("{} arguments:".format(len(argv) - 1))
     i = 1
