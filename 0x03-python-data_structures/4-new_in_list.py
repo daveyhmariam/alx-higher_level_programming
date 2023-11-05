774c6dfbe003d8d4e9ecda0317bcfbae26a4d7ab
daveyhmariam <daveyhmariam@gmail.com>
2023-11-05 10:47:10 +0300

    copy and replace a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/;python3
+def new_in_list(my_list, idx, element):
+    """
+
+    Non side-effect function
+
+    Args:
+       my_list: list
+       idx: index at which elemet is to be changed
+       element: item to replace
+
+    Returns: a copy of the input list
+    """
+    if idx < 0 or idx >= len(my_list):
+        return (my_list[:])
+    new_list = my_list[:]
+    new_list[idx] = element
+    return (new_list)
