5926cc7242fe60c575ef075a8f0e208ca6313384
daveyhmariam <daveyhmariam@gmail.com>
2023-11-05 11:31:51 +0300

    print_matrix_integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """
-    Print elements or matrix in order
 
-    Args:
-        matrix: input matrix
-    Return: None
-    """
     for r in matrix:
         for cl in r:
-            print("{:d}".format(cl), end=" ")
+            print("{:d}{}".format(cl, "" if cl == r[-1] else " " ), end="")
         print("")
