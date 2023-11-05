0b8b63e73a4660d17bca2d979d15ef145726c760
daveyhmariam <daveyhmariam@gmail.com>
2023-11-05 14:38:22 +0300

    check palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,12 +2,28 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *newhead, *reversed, *tmp, *prev;
+	listint_t *newhead, *reversed, *tmp, *prev, *fast, *slow;
 	if (*head == NULL || head == NULL)
 		return (1);
-	reversed = *head;
 	newhead = *head;
+	fast = *head;
+	slow = *head;
 	prev = NULL;
+	while (1)
+	{
+		if (fast->next)
+			fast = fast->next->next;
+		if (!fast)
+		{
+			break;
+		}
+		slow = slow->next;
+		
+	}
+
+	reversed = slow->next;
+	prev = NULL;
+	slow->next = NULL;
 	while (reversed)
 	{
 		tmp = reversed->next;
@@ -16,7 +32,7 @@ int is_palindrome(listint_t **head)
 		reversed = tmp;
 	}
 	reversed = prev;
-	while (reversed && newhead)
+	while (reversed)
 	{
 		if (reversed->n != newhead->n)
 			return (0);
