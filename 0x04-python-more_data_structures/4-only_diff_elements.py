9b51cb9c021d0a8c52a3ae101ab598304945b9bd
daveyhmariam <daveyhmariam@gmail.com>
2023-11-08 12:24:12 +0300

    symmeric difference

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """
+    Exclusive union of two sets
+
+    Args:
+        set_1: first set
+        set_2: second set
+
+    Return: exclusive union
+    """
+    return (set_1 ^ set_2)
