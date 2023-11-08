13796d9e72324950a7fc4e2a07ce2ebaf91a3b9d
daveyhmariam <daveyhmariam@gmail.com>
2023-11-08 13:28:28 +0300

    squaring matrix using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    if not matrix:
-        return matrix
     new_matrix = list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
     return new_matrix
