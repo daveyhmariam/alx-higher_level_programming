ca5c4d292a6078c3fb4df184780021583711da2a
daveyhmariam <daveyhmariam@gmail.com>
2023-11-08 12:25:08 +0300

    all keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns number of keys
+
+    Args:
+        a_dictionary: input dictionary
+    Return: number of keys
+    """
+    return len(a_dictionary.keys())
