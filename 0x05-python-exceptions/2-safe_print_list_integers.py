27ab6d3a3a9e0896aaf17d5abee980b50eaaa36e
daveyhmariam <daveyhmariam@gmail.com>
2023-11-20 23:06:35 +0300

    exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """Print list elements
+
+    Args:
+        my_list: list to print.
+
+    Returns:
+        number of printed elements
+    """
+    printed = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+        except (ValueError, TypeError)
+            continue
+    print("")
+    return printed
