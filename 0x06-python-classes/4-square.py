e49b4f8057f28640d1bd740fcacc4579b554ac00
daveyhmariam <daveyhmariam@gmail.com>
2023-11-21 23:15:27 +0300

    public method

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,10 +4,10 @@
 
 
 class Square:
-    """Represent a square."""
+    """a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
+        """Initi method of square.
 
         Args:
             size (int): The size of the new square.
@@ -16,7 +16,7 @@ class Square:
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
+        """Getter/setter the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -28,5 +28,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
+        """Return area of the square."""
         return (self.__size * self.__size)
