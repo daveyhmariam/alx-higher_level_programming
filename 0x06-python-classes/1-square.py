c5f8200616ae2a15b54b7c156c02fc909feb737a
daveyhmariam <daveyhmariam@gmail.com>
2023-11-21 22:37:19 +0300

    private instance argument

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class with private instance argument"""
+
+
+class Square():
+    """ Square class instance attribute"""
+
+    def __init__(self, size=0):
+        self.__size = size
