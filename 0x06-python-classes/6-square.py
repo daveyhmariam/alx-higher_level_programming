7fda96a4c39c031dfc0e0d6f5a9c98ab900622c2
daveyhmariam <daveyhmariam@gmail.com>
2023-11-22 19:00:38 +0300

    public method

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""class square"""
+
+
+class Square():
+
+    """ Square instance with input verification"""
+    def __init__(self, size=0, position=(0,0)):
+        """ initialiing method
+
+            Arg:
+                size(int): size of square
+                position (int, int): position of square
+        """
+
+        self.__size = size
+        self.__position = position
+    @property
+    def size(self):
+
+        """getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter method"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self)
+        
+        """getter method"""
+        return self.__position
+    
+    @position.setter
+    def position(self, value)
+        """position setter"""
+    
+        if (not all(isinstance(value, tuple)) or
+            not all([isinstance(i, int) for i in value]) or
+            not all(i >= 0 for i in tuple)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """ returns area of square"""
+        return self.__size ** 2
+    def my_print(self):
+
+        """Print # in square area"""
+
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
