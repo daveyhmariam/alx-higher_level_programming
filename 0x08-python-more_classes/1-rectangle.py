1db1e33c8fa7a3de5f396384b9ba259aced22a50
daveyhmariam <daveyhmariam@gmail.com>
2023-11-27 13:09:45 +0300

    update 0-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""
-Define A class
-"""
+"""Define A class"""
 
 
 class Rectangle:
-    """
-    Rectangle class that does nothing
-    """
+    """Rectangle class that does nothing"""
     def __init__(self, width=0, height=0):
         """
         Creates Rectangle Object
-        
+
         Args:
             width (int): width of rectangle
             height (int): height of rectangle
@@ -23,6 +19,7 @@ class Rectangle:
     def width(self):
         """property setter/getter"""
         return self.__width
+
     @width.setter
     def width(self, val):
         if not isinstance(val, int):
@@ -30,10 +27,12 @@ class Rectangle:
         if val < 0:
             raise ValueError("width must be >= 0")
         self.__width = val
+
     @property
     def height(self):
         """property setter/getter"""
         return self.__height
+
     @height.setter
     def height(self, val):
         if not isinstance(val, int):
