726c090c7df4c41b3dcc12b707962307f8273cc9
daveyhmariam <daveyhmariam@gmail.com>
2023-11-29 08:32:15 +0300

    update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+ The above class is a locked class that only
+ allows the attribute 'first_name' to be assigned.
+"""
+
+
 class LockedClass:
+
+    """
+    # The `__slots__` attribute is used to explicitly
+    define the attributes that a class can have. In
+    """
+
     __slots__ = ['first_name']
