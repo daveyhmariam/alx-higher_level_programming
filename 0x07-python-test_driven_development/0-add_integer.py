839299813e1bd0c21485b524506382d7454a2431
daveyhmariam <daveyhmariam@gmail.com>
2023-12-06 10:27:35 +0300

    update 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a function that adds two numbers"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers
+    Args:
+        a (int, float): integer argument
+        b (int, float): integer argument
+    return: sum
+    """
+    if (not isinstance(a, int) and
+            not isinstance(a, float)):
+        raise (TypeError("a must be an integer"))
+    if (not isinstance(b, int) and
+            not isinstance(b, float)):
+        raise (TypeError("b must be an integer"))
+    return int(a) + int(b)
