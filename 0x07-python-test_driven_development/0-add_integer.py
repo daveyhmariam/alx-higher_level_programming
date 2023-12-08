e1869cfd83b3ee79ddfb3685ed56ce383a5d3d15
daveyhmariam <daveyhmariam@gmail.com>
2023-12-08 08:33:58 +0300

    update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,12 +7,10 @@ def add_integer(a, b=98):
     Args:
         a (int, float): integer argument
         b (int, float): integer argument
-
-    Returns: sum
-    
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
 
+    Returns: sum
     """
     if (not isinstance(a, int) and
             not isinstance(a, float)):
