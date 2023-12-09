1e6b746da8eb723d94dc151c107ff38c4a71ee94
daveyhmariam <daveyhmariam@gmail.com>
2023-12-09 07:54:38 +0300

    update 7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Base class """
+
+
+class BaseGeometry:
+    """This class has two instance method (public)
+    that raises exception
+    """
+    def area(self):
+        """raises an exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validated integer input
+
+        Args:
+            name (str): name of value
+            value (int): value
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise TypeError(f"{name} must be greater than 0")
