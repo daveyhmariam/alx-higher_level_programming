281fe2428ebbc7624975b93d12b75e004aeea237
daveyhmariam <daveyhmariam@gmail.com>
2023-12-09 07:03:12 +0300

    update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@ a method to determine the class of an object
 
 
 def is_same_class(obj, a_class):
-    """checkes if an object is instance of a class 
+    """checkes if an object is instance of a class
 
     Args:
         obj (instance variable): reference to a object
