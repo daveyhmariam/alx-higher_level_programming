2a8f0a849830e108f7a77c305830e135395fa515
daveyhmariam <daveyhmariam@gmail.com>
2023-12-09 06:57:59 +0300

    update 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a method to determine the class of an object
+"""
+
+
+def is_same_class(obj, a_class):
+    """checkes if an object is instance of a class 
+
+    Args:
+        obj (instance variable): reference to a object
+        a_class (class): class name
+    Returns: True if type maches False otherwise
+    """
+    if type(obj) == a_class:
+        return True
+    return False
