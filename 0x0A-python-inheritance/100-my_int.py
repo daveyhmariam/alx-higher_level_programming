4c1784f4879d61c947ee26a7271c178968d65742
daveyhmariam <daveyhmariam@gmail.com>
2023-12-09 10:26:27 +0300

    update 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""MyInt class that inherits from int"""
+
+class MyInt(int):
+    """This class inherits from int and inverts the operators == and !="""
+    
+    def __eq__(self, other):
+        """invert the logical equal operator
+
+        Args:
+            other (object):
+        Return: self != other
+        """
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        """invert the logical not equal operator
+
+        Args:
+            other (object):
+        Return: self == other
+        """
+        return not super().__ne__(other)
