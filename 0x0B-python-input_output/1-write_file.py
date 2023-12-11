420be4747fbf1ec999be66471d508730d77f34fc
daveyhmariam <daveyhmariam@gmail.com>
2023-12-11 18:11:17 +0300

    update 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a function that writes a text file (UTF8) and prints it to stdout"""
+
+
+def write_file(filename="", text=""):
+    """writes a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str, optional): file to write to.defaults to "".
+        text (str, optional): txt to write.defaults to "".
+    """
+    if filename:
+        with open(filename, encoding="utf-8") as f:
+            f.write(text)
