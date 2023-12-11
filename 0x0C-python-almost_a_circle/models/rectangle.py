0b2263e299548a9910ef659483b72c03f31d46e0
daveyhmariam <daveyhmariam@gmail.com>
2023-12-12 01:03:48 +0300

    update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,7 +90,11 @@ class Rectangle(Base):
     def display(self):
         """Visual representation of the rectangle in stdout
         """
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
+            for j in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
