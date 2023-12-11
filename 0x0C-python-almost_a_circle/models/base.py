a7cd5e93475804f491c0beba849b06bdcf8a05f2
daveyhmariam <daveyhmariam@gmail.com>
2023-12-11 23:27:19 +0300

    update models README.md

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Base class for subsequent geometric classes"""
 
+
 class Base:
     """Base class for all geometric ssubclasses
     """
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """Object initializer
 
