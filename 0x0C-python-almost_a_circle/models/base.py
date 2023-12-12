be53623730d4be2858c32ba0f6cf03a4b952a04d
daveyhmariam <daveyhmariam@gmail.com>
2023-12-12 03:06:53 +0300

    update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,7 @@ class Base:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
+    @staticmethod            
     def to_json_string(list_dictionaries):
         """JSON string representation of list_dictionaries
 
