1a816f93250f67407289c0ca6dc111c74954adc7
daveyhmariam <daveyhmariam@gmail.com>
2023-12-15 21:41:48 +0300

    update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -104,7 +104,8 @@ class Base:
                 else:
                     fieldnames = ["id", "size", "x", "y"]
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
     @classmethod
     def load_from_file_csv(cls):
         """Return a list of classes instantiated from a CSV file.
