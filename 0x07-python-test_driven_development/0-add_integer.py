62388913151bbc4a4f8138b3b3c94420060049ea
daveyhmariam <daveyhmariam@gmail.com>
2024-01-11 22:07:27 +0300

    update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
 
     Returns: sum
     """
-    
+
     if not isinstance(a, (int, float)) or a is None:
         raise (TypeError("a must be an integer"))
     if not isinstance(b, (int, float)) or b is None:
