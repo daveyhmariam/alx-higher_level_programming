2ad089874c158df89793d3e42195feb32d734866
daveyhmariam <daveyhmariam@gmail.com>
2024-01-17 02:34:44 +0300

    first commit select with score constraint

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,6 @@
 -- select with score treshold
 
-select `score`, `name` from `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
