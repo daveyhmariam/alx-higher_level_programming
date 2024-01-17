8c51fdce545b607d5d0248bc214d81b6986afc87
daveyhmariam <daveyhmariam@gmail.com>
2024-01-17 03:26:18 +0300

    first commit convert to utf-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- convert to utf-8
 
-ALTER TABLE first_table TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table` CONERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
