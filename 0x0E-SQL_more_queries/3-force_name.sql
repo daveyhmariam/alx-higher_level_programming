0f9213ab29014bcbe55a29ddbf71efab29a9ac8c
daveyhmariam <daveyhmariam@gmail.com>
2024-01-19 01:06:16 +0300

    create table force_name fix

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,3 @@
--- create table
+-- create table force_name
 
-
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name(id INT NOT NULL, name VARCHAR(256));
