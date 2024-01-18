7e6f76333de0667cc42e04e4258d8c5da4ed86b2
daveyhmariam <daveyhmariam@gmail.com>
2024-01-19 00:56:41 +0300

    create user fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,7 @@
 -- create database and user with set privelages
 
-CREATE DATABASE IF NOT EXISTs hbtn_0d_2;
-CREAT USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
