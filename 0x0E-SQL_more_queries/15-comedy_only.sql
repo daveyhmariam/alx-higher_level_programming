b50b60000ba610c6c5634d5f10a33e7790aab72c
daveyhmariam <daveyhmariam@gmail.com>
2024-01-21 10:56:09 +0300

    lists all Comedy shows in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,5 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
-
-SELECT tv_shows.title, tv_genres.name FROM tv_shows
+SELECT tv_shows.title FROM tv_shows
 JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres
@@ -8,3 +7,4 @@ ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = "Comedy"
 Group BY tv_shows.title
 ORDER BY tv_shows.title ASC;
+
