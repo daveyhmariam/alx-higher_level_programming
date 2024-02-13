fee0c71a0d6ca490f95548b71412c122f48acdd7
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-02-13 14:25:46 +0300

    semistandard fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,10 @@
 
 const length = process.argv.length;
 
-if (length == 2)
-    console.log('No argument')
-else if (length == 3)
-    console.log('Argument found')
-else
-    console.log('Arguments found')
+if (length === 2) {
+  console.log('No argument');
+} else if (length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
