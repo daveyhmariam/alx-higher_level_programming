ba5b81a35fccc1954503d1511618c3c62606dc2f
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-02-20 10:13:38 +0300

    fix sorting function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,6 @@ if (len < 4) {
   console.log(0);
 } else {
   const arr = process.argv.slice(2).map(Number);
-  arr.sort(function(a, b){return a - b});
-  console.log(arr);
-  console.log(arr[len - 4]);
+  const second= arr.sort(function(a, b){return b - a})[1];
+  console.log(second);
 }
