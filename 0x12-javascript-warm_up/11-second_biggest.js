bc951de6bf93d42d6b3a96f971e115607b6698fc
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-02-21 09:20:21 +0300

    fix consol log

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const len = process.argv.length;
-if (len < 4) {
-  console.log(0);
+if (process.argv.length < 4) {
+  console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
   const second= arr.sort(function(a, b){return b - a})[1];
