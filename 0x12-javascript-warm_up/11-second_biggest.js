d2e6f248aca0ee4af0e572da6871e1a1c5934411
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-02-26 11:42:54 +0300

    fix unused variable

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const len = process.argv.length;
+
 if (process.argv.length < 4) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
-  const second= arr.sort(function(a, b){return b - a})[1];
+  const second = arr.sort(function (a, b) { return b - a; })[1];
   console.log(second);
 }
