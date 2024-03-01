b75a55d9032ee6c5c13d519ca33a4c0b99a3f78f
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-01 03:07:00 +0300

    script that sends a JSON POST request

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that sends a JSON POST request to a URL passed as the first argument
-curl -sH "content-type:application/json" -d "$(cat"$2")" "$1"
+curl -sH "Content-Type: application/json" -d "$(cat "$2")" "$1"
