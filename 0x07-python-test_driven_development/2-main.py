d0774bc41f95418c07edc0d7d5c99c3b8d3abc46
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-10 09:29:57 +0300

    divide matrix

---
+++ 0x07-python-test_driven_development/2-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [[1, 2, 3], [4, 5, 'a'], [7, 8, 9]], 2
+print(matrix_divided(matrix, 3))
+print(matrix)
