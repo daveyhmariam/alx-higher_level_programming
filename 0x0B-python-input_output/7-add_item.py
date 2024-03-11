e4ea5a7ed6bc3ddab51454cf0fb4262b8b84e723
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-11 22:49:24 +0300

    save argument list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""save list of arguments to a file"""
+import sys
+
+
+def add_item():
+    """add arguments to a jason file"""
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    try:
+        arg = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        arg = []
+    arg.extend(sys.argv[1:])
+    save_to_json_file(arg, "add_item.json")
+add_item()
