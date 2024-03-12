6b826b5b6d4ca12fbcb1b202107d1665bbd41207
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-12 09:45:40 +0300

    selected attributes

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """A Student class"""
+    def __init__(self, first_name, last_name, age):
+        """initialize Student object
+
+        Args:
+            first_name (str): first nae of student
+            last_name (str): last nam of student
+            age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return dict representatio of object
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+            all(type(el) == str for el in attrs)):
+            return ({k: getattr(self, k)} for k in attrs if hasattr(self, k))
+        return self.__dict__
