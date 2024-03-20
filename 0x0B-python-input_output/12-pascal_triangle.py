970220c13b2221428639e9cb2092c30acbf9d2f2
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-03-20 14:13:11 +0300

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""pascal triangle
+"""
+
+def pascal_triangle(n):
+    """ Pascal tringle using binomial theorem
+
+    Args:
+        n (int): level of pascal triangle
+    Return: list of lists
+    """
+    if (n <= 0):
+        return []
+    pascal = []
+    for i in range(n):
+        pascal.append([int(fact(i) / fact(i -l) / fact(l)) for l in range(i + 1)])
+    return pascal
+            
+def fact(i):
+    if (i <= 0):
+        return (1)
+    return (i * fact(i - 1))
