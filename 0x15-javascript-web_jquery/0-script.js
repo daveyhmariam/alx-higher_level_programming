7769c5e507fc67c023862a46f43b14cf2b53e71a
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-03 20:30:43 +0300

    semistandard global

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 /*  updates the text color of the <header> element to red (#FF0000) */
 
-document.querySelector("header").style.color = 'red'
+document.querySelector('header').style.color = 'red';
