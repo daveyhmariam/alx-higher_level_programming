f44c58a42e6c71ba81fae4a34fe95ffff7b0f5b9
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-05 11:50:24 +0300

    function definition update

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr
+  value: 12
+
 };
 console.log(myObject);
-function incr () {
+myObject.incr = function incr () {
   this.value++;
-}
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
