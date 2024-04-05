3e4bb10eb66f7161d2afe270edd2ffba618b1636
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-05 12:30:19 +0300

    reversed version of a list:

---
+++ 0x12-javascript-warm_up/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* returns the reversed version of a list */
+
+exports.esrever = function (list) {
+    let rev= [];
+    for (let i  = 0, j = list.length - 1; i < list.length; i++, j--) {
+        rev.push(list[j]);
+    }
+    return rev
+}
