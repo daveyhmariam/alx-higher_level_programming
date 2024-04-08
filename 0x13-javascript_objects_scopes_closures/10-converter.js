a761bcef66bc3651f674b05dd35b06ab5eb82aac
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-08 12:46:49 +0300

    convert base

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* a function that conveerts a base of number */
+
+exports.converter = function (base) {
+    return function (number) {
+        return number.toString(base)
+    }
+}
