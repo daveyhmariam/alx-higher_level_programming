7deadd07b12e683c1e9a45512ab8ef63b040f10b
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-10 08:56:44 +0300

    fix dict key

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,7 +5,7 @@ const dict = require('./101-data').dict;
 const newdict = {};
 for (const key of Object.keys(dict)) {
   const val = dict[key];
-  if (!newdict[key]) {
+  if (!newdict[val]) {
     newdict[val] = [];
   }
   newdict[val].push(key);
