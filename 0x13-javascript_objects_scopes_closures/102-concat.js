b214a53e1639c5f7b1b2eed44de7e688a1b7b40f
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-10 09:46:01 +0300

    concat sync

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,29 +4,6 @@
 const args = process.argv;
 const fs = require('fs');
 
-if (args.length !== 5) {
-  console.error('Usage: node concat_files.js input1.txt input2.txt output.txt');
-  process.exit(1);
-}
-
-fs.readFile(args[2], 'utf8', (err, data1) => {
-  if (err) {
-    console.error('Error reading file 1:', err);
-    return;
-  }
-
-  fs.readFile(args[3], 'utf8', (err, data2) => {
-    if (err) {
-      console.error('Error reading file 2:', err);
-      return;
-    }
-    const content = data1 + '\n' + data2;
-    fs.writeFile(args[4], content, (err) => {
-      if (err) {
-        console.error('Error writing to file:', err);
-        return;
-      }
-      console.log('Files concatenated successfully.');
-    });
-  });
-});
+const data1 = fs.readFileSync(args[2].toString())
+const data2 = fs.readFileSync(args[3].toString())
+fs.writeFileSync(args[4], data1 + '\n' + data2)
