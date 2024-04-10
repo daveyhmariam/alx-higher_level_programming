eaaf6cec9227da183df86c4cee607200cdfed69d
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-10 08:49:43 +0300

    imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+dict = require('./101-data').dict
+
+const newdict = {}
+for (const key of Object.keys(dict)) {
+    const val = dict[key]
+    if (!newdict[key]) {
+            newdict[val] = []
+    }
+    newdict[val].push(key)
+}
+
+console.log(dict)
+console.log(newdict)
