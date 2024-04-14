089bf8fc4690d7f53aad09b4bae426cb56685510
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-14 18:07:07 +0300

    style

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name,\
-        states.name FROM cities LEFT JOIN states ON states.id = cities.state_id\
+        states.name FROM cities LEFT JOIN states\
+        ON states.id = cities.state_id\
                 ORDER BY cities.id")
     rows = cur.fetchall()
     for row in rows:
