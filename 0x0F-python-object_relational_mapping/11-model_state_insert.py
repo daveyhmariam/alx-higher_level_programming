b7f13d453cde49224c3259fb3ccfeb9ae9433c0c
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-22 23:50:05 +0300

    adds the State object “Louisiana” to the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that adds the State object
+“Louisiana” to the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == "__main__":
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(State(name="Louisiana"))
+    session.commit()
