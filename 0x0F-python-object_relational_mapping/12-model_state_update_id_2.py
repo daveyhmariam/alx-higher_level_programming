3c1f5ce733961db065d61502e5f7a48fc20d658c
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-23 00:06:04 +0300

    changes the name of a State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+changes the name of a State object
+from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+
+if __name__ == "__main__":
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    row = session.query(State).filter(State.id == 3).first()
+    row.name = "New Mexico"
+    session.commit()
