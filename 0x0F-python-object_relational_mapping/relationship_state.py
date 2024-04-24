b92beacec1ff2335d97d484ff6842f29281c0b56
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-04-24 09:38:36 +0300

    class definition of states and city with relationship and script to add a city with state

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import relationship
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
 
+
 class State(Base):
     """
     class with attribute declaration of state object
@@ -17,4 +18,4 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    cities = relationship("City", backref="states", cascade="all")
