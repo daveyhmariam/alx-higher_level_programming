2adc4b36c0cfabcaa10d47b3833defe2220f7d42
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-01 19:46:45 +0300

    response from http response object

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,7 +9,7 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+        print(dict(res.headers).get('X-Request-Id'))
