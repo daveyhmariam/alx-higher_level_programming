e981f6309508a4325314716bf965c5d2ae1f2934
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-01 22:51:37 +0300

    fetch header requests package

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    res = requests.get(url)
+    print(res.headers.get('X-Request-Id'))
