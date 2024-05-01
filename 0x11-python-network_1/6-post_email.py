d7de9c7059b1b46cf0993f28f8e6f89cc4165d78
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-01 23:11:26 +0300

    akes in a URL and an email address, sends a POST request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,6 +9,6 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = {"email":sys.argv[2]}
+    email = {"email": sys.argv[2]}
     res = requests.post(url, data=email)
     print(res.text)
