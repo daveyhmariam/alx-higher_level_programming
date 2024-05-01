1128a2e2235047acf5a57811c7d8419de58935cb
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-01 22:17:24 +0300

    handle http error urllib

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     from urllib import request, error
 
     try:
-        with request.urlopen(argv[1]) as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+        url = argv[1]
+        with request.urlopen(url) as res:
+            pass
+    except error.URLError as e:
+        if hasattr(e, 'code'):
+            print("Error code: {}".format(e.code))
